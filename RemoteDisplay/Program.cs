7e678bc68f064cfe25eadbe98a0d4057c06c162f
Thomas Schissler <thomas@agilemax.de>
2021-11-13 12:11:12 +0100

    Temp

--- RemoteDisplay/Program.cs
+++ RemoteDisplay/Program.cs
@@ -1,5 +1,7 @@
+using NFLibs;
 using System;
 using System.Diagnostics;
+using System.Reflection;
 using System.Threading;
 
 namespace RemoteDisplay
@@ -8,6 +10,22 @@ namespace RemoteDisplay
     {
         public static void Main()
         {
+            Debug.WriteLine("");
+            Debug.WriteLine("#######################################");
+            Debug.WriteLine($"# Remote Display version {Assembly.GetExecutingAssembly().GetName().Version}");
+            Debug.WriteLine("#######################################");
+            Debug.WriteLine("");
+
+            Debug.Write("Connecting to Wifi ... ");
+            var isWifiConnected = WifiLib.ConnectToWifi(Secrets.Ssid, Secrets.Password);
+            if (!isWifiConnected)
+            {
+                Debug.WriteLine("");
+                Debug.WriteLine("Failed connecting to Wifi");
+                return;
+            }
+            Debug.WriteLine("Done");
+
 
         }
     }
