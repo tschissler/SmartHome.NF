811cad9f0eaed389cf0a8d217dd20fa1f8132afd
Thomas Schissler <thomas@agilemax.de>
2021-11-21 09:50:45 +0100

    First version deployed to Keller

--- SmartHome.NF/Program.cs
+++ SmartHome.NF/Program.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Device.Gpio;
+using System.Device.I2c;
 using System.Diagnostics;
 using System.Net.NetworkInformation;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading;
 using Iot.Device.Hcsr04;
+using Iot.Device.Shtc3;
 using nanoFramework.Azure.Devices.Client;
+using nanoFramework.Hardware.Esp32;
 using nanoFramework.Networking;
 using nanoFramework.Runtime.Native;
 using NFLibs;
@@ -20,16 +24,50 @@ namespace SmartHome.NF
         private static GpioPin IsAliveLED;
         private static GpioPin TransmitLED;
         private static Hcsr04 DistanceSensor;
+        private static Shtc3 TempHumiditySensor; 
         private static DeviceClient azureIoT;
         private static WebServer server = new WebServer();
         private static int connectedCount = 0;
-        public static string DeviceID = "M1_Keller";
+        private static int reedCounter = 0;
+        private static GpioPin reedContact;
+
+#if DEBUG
+        //Dev Board
+        private const string DeviceID = "DevBoard";
+        private const int Red_LED_Pin = 21;
+        private const int Green_LED_Pin = 23;
+        private const int Blue_LED_Pin = 22;
+        private const int DistanceSensor_Trigger_Pin = 14;
+        private const int DistanceSensor_Echo_Pin = 12;
+        private const int I2CDataPin = 18;
+        private const int I2CClockPin = 5;
+        private const int ReedPin = 19;
+        private static int TransmitInterval = (int)new TimeSpan(0, 0, 10).TotalMilliseconds;
+
+#else
+
+        // Prod Board
+        private const string DeviceID = "M1_Keller";
+        private const int Red_LED_Pin = 13;
+        private const int Green_LED_Pin = 12;
+        private const int Blue_LED_Pin = 27;
+        private const int DistanceSensor_Trigger_Pin = 15;
+        private const int DistanceSensor_Echo_Pin = 2;
+        private const int I2CDataPin = 32;
+        private const int I2CClockPin = 33;
+        private const int ReedPin = 19;
+        private static int TransmitInterval = (int)new TimeSpan(0, 10, 0).TotalMilliseconds;
+#endif
 
         public static void Main()
         {
             GpioController = new GpioController();
-            var startLED = GpioController.OpenPin(21, PinMode.Output);
+            var startLED = GpioController.OpenPin(Red_LED_Pin, PinMode.Output);
             startLED.Write(PinValue.High);
+            IsAliveLED = GpioController.OpenPin(Green_LED_Pin, PinMode.Output);
+            IsAliveLED.Write(PinValue.Low);
+            TransmitLED = GpioController.OpenPin(Blue_LED_Pin, PinMode.Output);
+            TransmitLED.Write(PinValue.Low);
 
             Debug.WriteLine("----- SmartHome.NF ------");
             Debug.WriteLine("Initializing...");
@@ -46,18 +84,42 @@ namespace SmartHome.NF
 
                 Debug.Write("   - GPIO...");
 
-                IsAliveLED = GpioController.OpenPin(22, PinMode.Output);
-                IsAliveLED.Write(PinValue.Low);
-                TransmitLED = GpioController.OpenPin(23, PinMode.Output);
-                TransmitLED.Write(PinValue.Low);
+
                 startLED.Write(PinValue.Low);
                 Timer blinkTimer = new Timer(IsAliveBlink, null, 1000, (int)new TimeSpan(0, 0, 3).TotalMilliseconds);
 
-                DistanceSensor = new Hcsr04(14, 12);
-                Timer distanceTimer = new Timer(MeassureDistance, null, 1000, (int)new TimeSpan(0,10,0).TotalMilliseconds);
+                DistanceSensor = new Hcsr04(DistanceSensor_Trigger_Pin, DistanceSensor_Echo_Pin);
+                Configuration.SetPinFunction(I2CDataPin, DeviceFunction.I2C1_DATA);
+                Configuration.SetPinFunction(I2CClockPin, DeviceFunction.I2C1_CLOCK);
+                I2cConnectionSettings settings = new I2cConnectionSettings(1, Shtc3.DefaultI2cAddress);
+                I2cDevice device = I2cDevice.Create(settings);
+                TempHumiditySensor = new Shtc3(device);
+
+                Timer transmitTimer = new Timer(TransmitDataToIotHub, null, 1000, TransmitInterval);
+
+                //GpioController.OpenPin(19, PinMode.Input);
+
+                reedContact = GpioController.OpenPin(ReedPin, PinMode.InputPullUp);
+                //reedContact.SetDriveMode(PinMode.InputPullUp);
+
+                // add a debounce timeout 
+                //reedContact.DebounceTimeout = new TimeSpan(0, 0, 0, 0, 100);
+                //reedContact.ValueChanged += TriggerReedContact;
+
+
+                GpioController.RegisterCallbackForPinValueChangedEvent(
+                    19,
+                    PinEventTypes.Falling | PinEventTypes.Rising,
+                    TriggerReedContact);
+
                 Debug.WriteLine("Done");
             }
             Thread.Sleep(Timeout.Infinite);
+        }
+
+        private static void TriggerReedContact(object sender, PinValueChangedEventArgs pinValueChangedEventArgs)
+        {
+            reedCounter++;
         }
 
         private static void ConnectWiFi()
@@ -119,19 +181,48 @@ namespace SmartHome.NF
             }
         }
 
-        private static void MeassureDistance(object state)
+        private static void TransmitDataToIotHub(object state)
         {
+            var message = new StringBuilder();
+            message.Append($"{{\"DeviceUTCTime\":\"{DateTime.UtcNow}\",\"deviceId\":\"{DeviceID}\",\"GasPulse\":{reedCounter}");
+
             if (DistanceSensor.TryGetDistance(out Length distance))
             {
-                TransmitLED.Write(PinValue.High);
+                message.Append($",\"ZisterneLevel\":{distance.Centimeters}");
                 Debug.WriteLine($"Distance: {distance.Centimeters} cm");
-                azureIoT.SendMessage($"{{\"DeviceUTCTime\":\"{DateTime.UtcNow}\",\"deviceId\":\"{DeviceID}\",\"ZisterneLevel\":{distance.Centimeters}}}");
-                TransmitLED.Write(PinValue.Low);
             }
             else
             {
-                Debug.WriteLine("Error reading sensor");
+                Debug.WriteLine("Error reading sensor for distance");
             }
+
+            if (TempHumiditySensor.TryGetTemperatureAndHumidity(out var temperature, out var relativeHumidity))
+            {
+                message.Append($",\"KellerTemp\":{temperature.DegreesCelsius},\"KellerHumidity\":{relativeHumidity.Percent}");
+                Debug.WriteLine($"Temp: {temperature.DegreesCelsius} °C");
+                Debug.WriteLine($"Humidity: {relativeHumidity.Percent} %");
+            }
+            else
+            {
+                Debug.WriteLine("Error reading temperature and humidity");
+            }
+
+            message.Append("}");
+
+            TransmitLED.Write(PinValue.High);
+            var t = message.ToString();
+            Debug.WriteLine(t);
+            if (azureIoT.SendMessage(message.ToString()))
+            {
+                Debug.Write("Data has been transmitted to Azure IoT Hub");
+            }
+            else
+            {
+                Debug.WriteLine("Error transmitting data to Azure IoT Hub");
+            }
+            reedCounter = 0;
+            TransmitLED.Write(PinValue.Low);
+
         }
         private static void IsAliveBlink(object state)
         {
@@ -142,6 +233,8 @@ namespace SmartHome.NF
             IsAliveLED.Toggle();
             Thread.Sleep(10);
             IsAliveLED.Toggle();
+
+            Debug.WriteLine($"{reedContact.Read().ToString()} - {reedCounter}");
         }
 
         /// <summary>
