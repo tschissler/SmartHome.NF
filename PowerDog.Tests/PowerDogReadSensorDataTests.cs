5aaf99ca69d1dad5bedf95c657c17fcd5baeff54
tschissler <tschissler@agilemax.de>
2022-09-17 12:21:43 +0200

    Changed PowerDog to .NET 6

--- PowerDog.Tests/PowerDogReadSensorDataTests.cs
+++ PowerDog.Tests/PowerDogReadSensorDataTests.cs
@@ -11,11 +11,11 @@ namespace PowerDog.Tests
         {
             Dictionary<string, string> sensorKeys = new()
             {
-                { "Bezug", "iec1107_1457430339" },
-                { "Erzeugung", "pv_global_1454412642" },
+                { "Bezug", "iec1107_1457430339" }, // Vom Zähler
+                { "Erzeugung", "pv_global_1454412642" },  // Vom Wechselrichter
                 { "Eigenverbrauch", "arithmetic_1457431399" },
                 { "Verbrauchgesamt", "arithmetic_1457432629" },
-                { "lieferung", "iec1107_1457430562" }
+                { "lieferung", "iec1107_1457430562" } // Vom Zähler
             };
 
             UriBuilder uri = new ("http", "192.168.178.150", 20000);
