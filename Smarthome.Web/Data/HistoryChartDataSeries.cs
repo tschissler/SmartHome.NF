01344e8738b09f577d4b1a4317d5e388475da5d4
tschissler <tschissler@agilemax.de>
2022-10-08 14:18:01 +0200

    Aded customization for color and type to history chart

---
+++ Smarthome.Web/Data/HistoryChartDataSeries.cs
@@ -0,0 +1,14 @@
+﻿using SharedContracts.DataPoints;
+using Syncfusion.Blazor.Charts;
+
+namespace Smarthome.Web.Data
+{
+    public class HistoryChartDataSeries
+    {
+        public string Name { get; set; }
+        public DecimalDataPoint DataPoint { get; set; }
+        public ChartSeriesType Type { get; set; }
+        public double Opacity { get; set; }
+        public string Color { get; set; }
+    }
+}
