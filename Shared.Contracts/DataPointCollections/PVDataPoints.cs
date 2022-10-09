b3cf9f0bcdb10bcde19c5344f9b9be56dda67dc0
tschissler <tschissler@agilemax.de>
2022-10-09 12:40:23 +0200

    Changed length of history for PVControllerDataPoints

--- Shared.Contracts/DataPointCollections/PVDataPoints.cs
+++ Shared.Contracts/DataPointCollections/PVDataPoints.cs
@@ -4,8 +4,8 @@ namespace SharedContracts.DataPointCollections
 {
     public class PVDataPoints
     {
-        public DecimalDataPoint PVProduction = new() { Label = "Produktion",  Unit = "W", MaxValue = 12000, DecimalDigits = 1, History = new() { DataHistoryLength = 100 }, CurrentValueCorrection = (5.7 + 4.57) / 5.7 };
-        public DecimalDataPoint GridSupply   = new() { Label = "Einspeisung", Unit = "W", MaxValue = 12000, DecimalDigits = 1, History = new() { DataHistoryLength = 100 } };
-        public DecimalDataPoint GridDemand   = new() { Label = "Bezug",       Unit = "W", MaxValue = 8000,  DecimalDigits = 1, History = new() { DataHistoryLength = 100 } };
+        public DecimalDataPoint PVProduction = new() { Label = "Produktion",  Unit = "W", MaxValue = 12000, DecimalDigits = 1, History = new() { DataHistoryLength = 200 }, CurrentValueCorrection = (5.7 + 4.57) / 5.7 };
+        public DecimalDataPoint GridSupply   = new() { Label = "Einspeisung", Unit = "W", MaxValue = 12000, DecimalDigits = 1, History = new() { DataHistoryLength = 200 } };
+        public DecimalDataPoint GridDemand   = new() { Label = "Bezug",       Unit = "W", MaxValue = 8000,  DecimalDigits = 1, History = new() { DataHistoryLength = 200 } };
     }
 }
