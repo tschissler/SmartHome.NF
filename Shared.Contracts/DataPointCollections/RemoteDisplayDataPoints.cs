872ee0f93e236e8bda39eaa5b8a8f770b5065aa1
tschissler <tschissler@agilemax.de>
2022-10-09 17:12:25 +0200

    Update aggregation interval

--- Shared.Contracts/DataPointCollections/RemoteDisplayDataPoints.cs
+++ Shared.Contracts/DataPointCollections/RemoteDisplayDataPoints.cs
@@ -9,9 +9,9 @@ namespace SharedContracts.DataPointCollections
 {
     public class RemoteDisplayDataPoints
     {
-        public DecimalDataPoint Temperature = new() { Label="Temperatur", Unit="°C", MaxValue=30, DecimalDigits = 1, History = new() { DataHistoryLength = 100, AggregationTimeSpan = TimeSpan.FromMinutes(10), AggregationHistoryLength = 144, AggregationType = AggregationType.Average } };
-        public DecimalDataPoint Humidity = new() { Label = "Luftfeuchtigkeit", Unit = "%", MaxValue = 100, DecimalDigits = 1, History = new() { DataHistoryLength = 100, AggregationTimeSpan = TimeSpan.FromMinutes(10), AggregationHistoryLength = 144, AggregationType = AggregationType.Average } };
-        public DecimalDataPoint Pressure = new() { Label = "Luftdruck", Unit = "hPa", MaxValue = 1100, DecimalDigits = 0, History = new() { DataHistoryLength = 100, AggregationTimeSpan = TimeSpan.FromMinutes(10), AggregationHistoryLength = 144, AggregationType = AggregationType.Average } };
-        public DecimalDataPoint Illumination = new() { Label = "Beleuchtungsstärke", Unit = "lx", MaxValue = 1000, DecimalDigits = 0, History = new() { DataHistoryLength = 100, AggregationTimeSpan = TimeSpan.FromMinutes(10), AggregationHistoryLength = 144, AggregationType = AggregationType.Average } };
+        public DecimalDataPoint Temperature  = new() { Label = "Temperatur",         Unit="°C",    MaxValue = 30,   DecimalDigits = 1, History = new() { DataHistoryLength = 100, AggregationTimeSpan = TimeSpan.FromMinutes(1), AggregationHistoryLength = 1440, AggregationType = AggregationType.Average } };
+        public DecimalDataPoint Humidity     = new() { Label = "Luftfeuchtigkeit",   Unit = "%",   MaxValue = 100,  DecimalDigits = 1, History = new() { DataHistoryLength = 100, AggregationTimeSpan = TimeSpan.FromMinutes(1), AggregationHistoryLength = 1440, AggregationType = AggregationType.Average } };
+        public DecimalDataPoint Pressure     = new() { Label = "Luftdruck",          Unit = "hPa", MaxValue = 1100, DecimalDigits = 0, History = new() { DataHistoryLength = 100, AggregationTimeSpan = TimeSpan.FromMinutes(1), AggregationHistoryLength = 1440, AggregationType = AggregationType.Average } };
+        public DecimalDataPoint Illumination = new() { Label = "Beleuchtungsstärke", Unit = "lx",  MaxValue = 1000, DecimalDigits = 0, History = new() { DataHistoryLength = 100, AggregationTimeSpan = TimeSpan.FromMinutes(1), AggregationHistoryLength = 1440, AggregationType = AggregationType.Average } };
     }
 }
