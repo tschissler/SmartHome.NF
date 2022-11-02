8b3cc3654ae83c9362dd9b5060b1fc58a726daec
tschissler <tschissler@agilemax.de>
2022-11-02 22:09:36 +0100

    Changed rounding of historydatarows

--- Shared.Contracts/DataPoints/HistoryDataRow.cs
+++ Shared.Contracts/DataPoints/HistoryDataRow.cs
@@ -1,5 +1,28 @@
-﻿namespace SharedContracts.DataPoints
+﻿
+namespace SharedContracts.DataPoints
 {
+    public static class DateTimeExtensions
+    {
+        public static DateTime Floor(this DateTime dateTime, TimeSpan interval)
+        {
+            return dateTime.AddTicks(-(dateTime.Ticks % interval.Ticks));
+        }
+
+        public static DateTime Ceiling(this DateTime dateTime, TimeSpan interval)
+        {
+            var overflow = dateTime.Ticks % interval.Ticks;
+
+            return overflow == 0 ? dateTime : dateTime.AddTicks(interval.Ticks - overflow);
+        }
+
+        public static DateTime Round(this DateTime dateTime, TimeSpan interval)
+        {
+            var halfIntervalTicks = (interval.Ticks + 1) >> 1;
+
+            return dateTime.AddTicks(halfIntervalTicks - ((dateTime.Ticks + halfIntervalTicks) % interval.Ticks));
+        }
+    }
+    
     public class HistoryDataPoint
     {
         public DateTime Timestamp { get; set; }
@@ -18,7 +41,7 @@
     public class HistoryDataRow
     {
         public List<HistoryDataPoint> DataHistory { get; private set; }
-        public List<HistoryDataPoint> AggregatedHistory { get; private set; }
+        public List<HistoryDataPoint> AggregatedHistory { get; set; }
         public int DataHistoryLength { get; set; }
         public int AggregationHistoryLength { get; set; }
         public TimeSpan AggregationTimeSpan { get; set; }
@@ -39,7 +62,7 @@
             {
                 DataHistory.RemoveAt(0);
             }
-            DataHistory.Add(new HistoryDataPoint { Timestamp = DateTime.Now, Value = dataPoint });
+            DataHistory.Add(new HistoryDataPoint { Timestamp = DateTime.Now.Floor(new TimeSpan(0,0,1)), Value = dataPoint });
 
             if (AggregationHistoryLength > 0 && 
                 AggregationTimeSpan.TotalMilliseconds > 0 && 
@@ -48,24 +71,24 @@
                 switch (AggregationType)
                 {       
                     case AggregationType.None:
-                        AggregatedHistory.Add(new HistoryDataPoint { Timestamp = DateTime.Now, Value = DataHistory.Where(i => i.Timestamp > lastAggregationTimestamp).Last().Value });
+                        AggregatedHistory.Add(new HistoryDataPoint { Timestamp = DateTime.Now.Floor(new TimeSpan(0, 0, 10)), Value = DataHistory.Where(i => i.Timestamp > lastAggregationTimestamp).Last().Value });
                         break;
                     case AggregationType.Average:
-                        AggregatedHistory.Add(new HistoryDataPoint { Timestamp = DateTime.Now, Value = DataHistory.Where(i => i.Timestamp > lastAggregationTimestamp).Average(x => x.Value) });
+                        AggregatedHistory.Add(new HistoryDataPoint { Timestamp = DateTime.Now.Floor(new TimeSpan(0, 0, 10)), Value = DataHistory.Where(i => i.Timestamp > lastAggregationTimestamp).Average(x => x.Value) });
                         break;
                     case AggregationType.Sum:
-                        AggregatedHistory.Add(new HistoryDataPoint { Timestamp = DateTime.Now, Value = DataHistory.Where(i => i.Timestamp > lastAggregationTimestamp).Sum(x => x.Value) });
+                        AggregatedHistory.Add(new HistoryDataPoint { Timestamp = DateTime.Now.Floor(new TimeSpan(0, 0, 10)), Value = DataHistory.Where(i => i.Timestamp > lastAggregationTimestamp).Sum(x => x.Value) });
                         break;
                     case AggregationType.Min:
-                        AggregatedHistory.Add(new HistoryDataPoint { Timestamp = DateTime.Now, Value = DataHistory.Where(i => i.Timestamp > lastAggregationTimestamp).Min(x => x.Value) });
+                        AggregatedHistory.Add(new HistoryDataPoint { Timestamp = DateTime.Now.Floor(new TimeSpan(0, 0, 10)), Value = DataHistory.Where(i => i.Timestamp > lastAggregationTimestamp).Min(x => x.Value) });
                         break;
                     case AggregationType.Max:
-                        AggregatedHistory.Add(new HistoryDataPoint { Timestamp = DateTime.Now, Value = DataHistory.Where(i => i.Timestamp > lastAggregationTimestamp).Max(x => x.Value) });
+                        AggregatedHistory.Add(new HistoryDataPoint { Timestamp = DateTime.Now.Floor(new TimeSpan(0, 0, 10)), Value = DataHistory.Where(i => i.Timestamp > lastAggregationTimestamp).Max(x => x.Value) });
                         break;
                     default:
                         break;
                 }
-                AggregatedHistory.Add(new HistoryDataPoint { Timestamp = DateTime.Now, Value = DataHistory.Where(i => i.Timestamp > lastAggregationTimestamp).Average(x => x.Value) });
+                //AggregatedHistory.Add(new HistoryDataPoint { Timestamp = DateTime.Now, Value = DataHistory.Where(i => i.Timestamp > lastAggregationTimestamp).Average(x => x.Value) });
                 lastAggregationTimestamp = DateTime.Now;
                 if (AggregatedHistory.Count >= AggregationHistoryLength)
                 {
