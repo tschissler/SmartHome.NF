c1ebe20581eece2c99a8113b0083e3cd0ffd0946
tschissler <tschissler@agilemax.de>
2022-12-30 14:18:40 +0100

    Initial version of Blazor MAUI App

---
+++ Smarthome.App/Data/DeviceOrientationService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Smarthome.App.Data;
+
+#if ANDROID
+using Android.Content;
+using Android.Views;
+using Android.Runtime;
+#elif IOS
+using UIKit;
+#endif
+
+
+
+namespace Smarthome.App.Data
+{
+    public class DeviceOrientationService
+    {
+        public DeviceOrientation GetOrientation()
+        {
+#if ANDROID
+            IWindowManager windowManager = Android.App.Application.Context.GetSystemService(Context.WindowService).JavaCast<IWindowManager>();
+            SurfaceOrientation orientation = windowManager.DefaultDisplay.Rotation;
+            bool isLandscape = orientation == SurfaceOrientation.Rotation90 || orientation == SurfaceOrientation.Rotation270;
+            return isLandscape ? DeviceOrientation.Landscape : DeviceOrientation.Portrait;
+#elif IOS
+            UIInterfaceOrientation orientation = UIApplication.SharedApplication.StatusBarOrientation;
+            bool isPortrait = orientation == UIInterfaceOrientation.Portrait || orientation == UIInterfaceOrientation.PortraitUpsideDown;
+            return isPortrait ? DeviceOrientation.Portrait : DeviceOrientation.Landscape;
+#else
+            return DeviceOrientation.Undefined;
+#endif
+        }
+    }
+}
---
+++ Smarthome.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Smarthome.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
