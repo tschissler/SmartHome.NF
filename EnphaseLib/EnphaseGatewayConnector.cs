853aa3c94ec331ff2323a1e27f6a6f459c73601f
Thomas Schissler <tschissler@agilemax.de>
2023-09-17 23:47:15 +0200

    Added testservice

---
+++ EnphaseLib/EnphaseGatewayConnector.cs
@@ -0,0 +1,33 @@
+﻿using NEnvoy;
+using NEnvoy.Models;
+
+namespace EnphaseLib
+{
+    public class EnphaseGatewayConnector
+    {
+        private static EnvoyClient client; 
+        //static EnphaseConnector()
+        //{
+        //    //client = Login().Result;
+        //}
+
+        private static async Task<EnvoyClient> Login()
+        {
+            var ci = new EnvoyConnectionInfo()
+            {
+                Username = Secrets.EnphaseSecrets.UserName,
+                Password = Secrets.EnphaseSecrets.Password,
+                EnvoyHost = "envoy.local",
+                EnphaseEntrezBaseUri = "https://entrez.enphaseenergy.com/"
+            };
+            return await EnvoyClient.FromLoginAsync(ci).ConfigureAwait(false);
+        }
+
+        public static IEnumerable<RootMeterReading> GetMeterReadings()
+        {
+            var c = Login().Result;
+            return c.GetMeterReadingsAsync().Result;
+        }
+
+    }
+}
---
+++ EnphaseLibTests/EnphaseConnectorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EnphaseLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnphaseLibTests
+{
+    [TestClass()]
+    public class EnphaseConnectorTests
+    {
+        [TestMethod()]
+        public void GetMeterReadingsTest()
+        {
+            var result = EnphaseGatewayConnector.GetMeterReadings();
+        }
+    }
+}
