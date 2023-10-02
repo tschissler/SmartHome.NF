dd591b30458c9ad9b4167e6326c06b2c9f8398a0
Thomas Schissler <tschissler@agilemax.de>
2023-10-02 21:44:29 +0200

    Added new configuration for PowerDog

---
+++ Shared.Contracts/Configuration/PowerDog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedContracts.Configuration
+{
+    public class PowerDog
+    { 
+        public static string Password { get; set; }
+
+        static PowerDog()
+        {
+            if (Environment.GetEnvironmentVariable("PowerDogPassword") is string password)
+            {
+                Password = password;
+            }
+            else
+            {
+                throw new Exception("EnvironmentVariable PowerDogPassword not set, access to PowerDog will not be possible.");
+            }
+        }
+    }
+}
